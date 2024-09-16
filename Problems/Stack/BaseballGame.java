3f26a6b060db14a6d58c9a0aa41d0c67b8cac254
Dhaya <dhayagithub@gmail.com>
2024-09-16 22:29:14 +0530

    Solved two easy problems on stack

---
+++ Problems/Stack/BaseballGame.java
@@ -0,0 +1,27 @@
+package Stack;
+
+import java.util.Stack;
+
+public class BaseballGame {
+
+    // using array is simple but learned to implement stack
+    public static int calPoints(String[] operations) {
+        Stack<Integer> result = new Stack<>();
+
+        for (String operation : operations) {
+            switch (operation) {
+                case "+" -> result.push(result.lastElement() + result.get(result.size() - 2));
+                case "C" -> result.pop();
+                case "D" -> result.push(2 * result.lastElement());
+                default -> result.push(Integer.valueOf(operation));
+            }
+        }
+
+        return result.stream().mapToInt(Integer::intValue).sum();
+    }
+
+    public static void main(String[] args) {
+        String[] operations = {"5","2","C","D","+"};
+        System.out.println(calPoints(operations));
+    }
+}
---
+++ Problems/Stack/RemoveAllAdjacentDuplicatesInString.java
@@ -0,0 +1,25 @@
+package Stack;
+
+import java.util.Stack;
+import java.util.stream.Collectors;
+
+public class RemoveAllAdjacentDuplicatesInString {
+
+    public static String removeDuplicates(String s) {
+        Stack<Character> strings = new Stack<>();
+
+        for (Character c : s.toCharArray()) {
+            if (strings.isEmpty()) strings.push(c);
+            else
+                if (c.equals(strings.lastElement())) strings.pop();
+                else strings.push(c);
+        }
+
+        return strings.stream().map(String::valueOf).collect(Collectors.joining());
+    }
+
+    public static void main(String[] args) {
+        System.out.println(removeDuplicates("abbaca"));
+        System.out.println(removeDuplicates("azxxzy"));
+    }
+}
