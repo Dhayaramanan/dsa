d3fa2870f9514ee099195eab1095d531ea3339e3
Dhaya <dhayagithub@gmail.com>
2025-06-16 09:13:13 +0530

    Changed variable name to camelCase

--- Problems/Array/MaximumDifferenceBetweenIncreasingElements.java
+++ Problems/Array/MaximumDifferenceBetweenIncreasingElements.java
@@ -2,18 +2,18 @@ package Array;
 
 public class MaximumDifferenceBetweenIncreasingElements {
     public static int maximumDifference(int[] nums) {
-        int max_diff = -1;
+        int maxDiff = -1;
         int min = nums[0];
 
         for (int i = 1; i < nums.length; i++) {
             if (min >= nums[i]) min = nums[i];
             else {
                 int diff = nums[i] - min;
-                max_diff = Math.max(diff, max_diff);
+                maxDiff = Math.max(diff, maxDiff);
             }
         }
 
-        return max_diff;
+        return maxDiff;
     }
 
     public static void main(String[] args) {
